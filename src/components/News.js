eea1bd84f0294ec58f92e41f9c2d9059a13714fc
shivam negi <52135430+shivamnegi479@users.noreply.github.com>
2022-05-24 09:35:49 +0530

    24 april first commit

--- src/components/News.js
+++ src/components/News.js
@@ -18,62 +18,36 @@ export class News extends Component {
 
   }
 
-  articles=[
-    {
-        "source": {
-            "id": "bbc-sport",
-            "name": "BBC Sport"
-        },
-        "author": "BBC Sport",
-        "title": "Shane Warne memorial - watch & follow updates",
-        "description": "Watch live coverage and follow text updates and tributes from the state memorial for Australian cricket legend Shane Warne at the Melbourne Cricket Ground.",
-        "url": "http://www.bbc.co.uk/sport/live/cricket/60916236",
-        "urlToImage": "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png",
-        "publishedAt": "2022-03-30T08:22:26.498888Z",
-        "content": "Former England bowler and BBC cricket presenter Isa Guha, who became a colleague of Warne's in the commentary box: \"It has been a strange few weeks - a lot of shock and then we did our own tribute at… [+396 chars]"
-    },
-    {
-        "source": {
-          "id": "espn-cric-info",
-          "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-        "source": {
-          "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-          },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ]
-  constructor(){
-    super();
+  articles=[]
+  constructor(props){
+    super(props);
+let myt=this.props.category
 
     this.state={
       articles:this.articles,
       loading:false,
       page:1
     }
-    
+    document.title=`${myt.charAt(0).toUpperCase()+myt.slice(1)} - News_App`
+
+  }
+
+  async updateNews(){
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c2cffc376cf841fda2e39392378a1c2e&page=${this.state.page}&pageSize=${this.props.pagesize}`
+    this.setState({loading:true})
+    let data=await fetch(url)
+   
+    let parseData=await data.json();
+    //  console.log(parseData)
+    this.setState({articles:parseData.articles,
+     totalResults:parseData.totalResults,
+     loading:false})
   }
   async componentDidMount(){
     
  let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c2cffc376cf841fda2e39392378a1c2e&page=1&pageSize=${this.props.pagesize}`
- let data=await fetch(url)
  this.setState({loading:true})
+ let data=await fetch(url)
 
  let parseData=await data.json();
  //  console.log(parseData)
@@ -83,15 +57,19 @@ export class News extends Component {
 }
 
 prevClick= async ()=>{
-  let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c2cffc376cf841fda2e39392378a1c2e&page=${this.state.page-1}&pageSize=${this.props.pagesize}`
-  let data=await fetch(url)
-  this.setState({loading:true})
+  // let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c2cffc376cf841fda2e39392378a1c2e&page=${this.state.page-1}&pageSize=${this.props.pagesize}`
+  // this.setState({loading:true})
+  // let data=await fetch(url)
 
-  let parseData=await data.json();
+  // let parseData=await data.json();
+  // this.setState({
+  //  articles:parseData.articles,
+  //  page:this.state.page-1,
+  //  loading:false
+  // })
+  this.updateNews();
   this.setState({
-   articles:parseData.articles,
-   page:this.state.page-1,
-   loading:false
+    page:this.state.page-1
   })
 }
 nextClick= async ()=>{
@@ -99,23 +77,29 @@ nextClick= async ()=>{
     
   }
   else{  
-      let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c2cffc376cf841fda2e39392378a1c2e&page=${this.state.page+1}&pageSize=${this.props.pagesize}`
-      let data=await fetch(url)
-      this.setState({loading:true})
-      let parseData=await data.json();
-      // console.log(parseData)
-      this.setState({
-        page:this.state.page+1,
-        articles:parseData.articles,
-        loading:false
- })
-}
+//       let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c2cffc376cf841fda2e39392378a1c2e&page=${this.state.page+1}&pageSize=${this.props.pagesize}`
+//       this.setState({loading:true})
+//       let data=await fetch(url)
+//       let parseData=await data.json();
+//       // console.log(parseData)
+//       this.setState({
+//         page:this.state.page+1,
+//         articles:parseData.articles,
+//         loading:false
+//  })
+// }
+this.updateNews();
+this.setState({
+  page:this.state.page+1
+})
+  }
 }
   render() {
 
+    let myt1=this.props.category
        return (
          <div className='container my-3'>
-          <h3>Welcome To news Monkey</h3>
+          <h3 className='text-center my-4'>News App - Top {myt1.charAt(0).toUpperCase()+myt1.slice(1)} Headlines</h3>
           
           <div className="row ">
           <div className='text-center spinner'>
@@ -126,7 +110,7 @@ nextClick= async ()=>{
               
               
               >
-              <NewsItem   title={element.title.substring(0,30)} description={element.content?element.content.substring(0,80):("Demo Demo demo demo demo Demo Demo demo demo demo Demo Demo demo demo demo Demo Demo demo demo demo Demo Demo demo demo demo").substring(0,80)} imgUrl={element.urlToImage?element.urlToImage:"https://www.businessinsider.in/photo/91462028/watch-astronaut-samantha-cristoforetti-creates-history-with-first-tiktok-video-from-international-space-station.jpg?imgsize=32384"} newsurl={element.url}/>
+              <NewsItem source={element.source.name} author={element.author?element.author:"Shivam"} date={element.publishedAt}  title={element.title.substring(0,30)} description={element.content?element.content.substring(0,80):("Demo Demo demo demo demo Demo Demo demo demo demo Demo Demo demo demo demo Demo Demo demo demo demo Demo Demo demo demo demo").substring(0,80)} imgUrl={element.urlToImage?element.urlToImage:"https://www.businessinsider.in/photo/91462028/watch-astronaut-samantha-cristoforetti-creates-history-with-first-tiktok-video-from-international-space-station.jpg?imgsize=32384"} newsurl={element.url}/>
               </div>
             })}
        
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,16 +3,22 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
  
   render() {
-      let {title, description,imgUrl,newsurl}=this.props
+      let {title, description,imgUrl,newsurl,author,date,source}=this.props
       // let description1=toString(description).slice(0,50)
       
     return (
       <div>
+      
         <div className="card" >
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:"90%",zIndex:1}}>
+   {source}
+
+  </span>
         <img src={imgUrl} className="card-img-top" alt="..." />
         <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">{title}..</h5>
             <p className="card-text">{description}...</p>
+            <p className="card-text second-text"><small className='text-muted'>By {author} {new Date(date).toUTCString()}</small></p>
             <a rel="noreferrer" href={newsurl} target="_blank" className="btn btn-info">Know More</a>
         </div>
         </div>
